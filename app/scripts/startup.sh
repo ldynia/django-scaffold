47fd6ae8c69512b3f54e800adf642edb172d6271
ldynia <l_dynowski@yahoo.com>
2020-12-01 23:42:04 +0100

    removing sleep

--- app/scripts/startup.sh
+++ app/scripts/startup.sh
@@ -1,10 +1,6 @@
 #!/bin/ash
 
-pip install --upgrade pip --no-cache-dir
 pip install -r /app/requirements.txt --no-cache-dir
 
 python /app/manage.py migrate
-#python /app/manage.py createsuperuser
 python /app/manage.py runserver 0.0.0.0:8000
-
-sleep 3600
