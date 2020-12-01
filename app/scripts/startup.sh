fd663df516a5f7c224c1026f59a54914f08b7e41
ldynia <l_dynowski@yahoo.com>
2020-12-01 23:24:07 +0100

    making django secure

--- app/scripts/startup.sh
+++ app/scripts/startup.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/bin/ash
+
+pip install --upgrade pip --no-cache-dir
+pip install -r /app/requirements.txt --no-cache-dir
 
 python /app/manage.py migrate
 #python /app/manage.py createsuperuser
-python /app/manage.py runserver 0.0.0.0:80
+python /app/manage.py runserver 0.0.0.0:8000
+
+sleep 3600
