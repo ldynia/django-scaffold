4d2da1d4a90e05470428c174de822b591bf1d3f4
ldynia <l_dynowski@yahoo.com>
2020-12-07 21:45:56 +0100

    Update startup.sh

--- app/scripts/startup.sh
+++ app/scripts/startup.sh
@@ -4,7 +4,9 @@ echo "Install requirements.txt"
 pip install --upgrade pip --no-cache-dir
 pip install -r /app/requirements.txt --no-cache-dir
 
-echo "Run server"
-# python /app/manage.py migrate
+echo "Run migrations"
+python /app/manage.py migrate
 # python /app/manage.py createsuperuser
+
+echo "Run server"
 python /app/manage.py runserver 0.0.0.0:8000
